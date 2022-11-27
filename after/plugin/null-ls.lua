ca214b03aa481458fef2ea691d069d9387be9688
Carlton Joseph <carlton.joseph@gmail.com>
2022-11-27 11:03:34 -0800

    disable eslint on startup

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -4,7 +4,7 @@ local formatting = null_ls.builtins.formatting
 -- source below from github directory lua/null-ls/builtins/
 null_ls.setup({
 	sources = {
-		null_ls.builtins.diagnostics.eslint,
+		-- null_ls.builtins.diagnostics.eslint,
 		-- null_ls.builtins.completion.spell,
 		formatting.stylua,
 		formatting.prettierd,
