20fe2aaf7237d1d4de00b39b3910ec7bea683b0c
Carlton Joseph <carlton.joseph@gmail.com>
2022-12-02 10:05:49 -0800

    add esc keybinding

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,8 @@
 local keymap = vim.keymap
 
 keymap.set("n", "<F1>", "<cmd>echo<cr>")
+keymap.set("i", "<F1>", "<esc><cmd>echo<cr>")
 keymap.set("n", "<c-a>", "ggvG$")
-keymap.set("t", "hh", "<c-\\><c-n>")
+keymap.set("t", "<<", "<c-\\><c-n>")
+keymap.set("i", "jk", "<esc>")
+keymap.set("i", "kj", "<esc>")
