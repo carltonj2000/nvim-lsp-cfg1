4d0907f51fabdd249745e9bc0fb6bdfa285ced28
Carlton Joseph <carlton.joseph@gmail.com>
2022-12-17 11:56:01 -0800

    fix transparent bg for telescope

--- lua/base.lua
+++ lua/base.lua
@@ -17,6 +17,7 @@ function ColorSet()
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
 end
 
 ColorSet()
