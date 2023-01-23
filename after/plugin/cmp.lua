74c8ba7b05052894eb919d9ca9d0bfc89d3e22d5
Carlton Joseph <carlton.joseph@gmail.com>
2023-01-23 12:05:59 -0800

    update cmp mapping

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -5,7 +5,7 @@ vscode_ldr.lazy_load()
 
 local status, cmp = pcall(require, "cmp")
 if not status then
-  return
+	return
 end
 
 local luasnip = require("luasnip")
@@ -13,124 +13,124 @@ local luasnip = require("luasnip")
 local select_opts = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  sources = {
-    { name = "luasnip" },
-    { name = "nvim_lsp" },
-    { name = "path" },
-    { name = "buffer" },
-  },
-  window = {
-    documentation = cmp.config.window.bordered(),
-  },
-  formatting = {
-    fields = { "menu", "abbr", "kind" },
-    format = function(entry, item)
-      local menu_icon = {
-        nvim_lsp = "λ",
-        luasnip = "⋗",
-        buffer = "Ω",
-        path = "🖫",
-      }
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	sources = {
+		{ name = "luasnip" },
+		{ name = "nvim_lsp" },
+		{ name = "path" },
+		{ name = "buffer" },
+	},
+	window = {
+		documentation = cmp.config.window.bordered(),
+	},
+	formatting = {
+		fields = { "menu", "abbr", "kind" },
+		format = function(entry, item)
+			local menu_icon = {
+				nvim_lsp = "λ",
+				luasnip = "⋗",
+				buffer = "Ω",
+				path = "🖫",
+			}
 
-      item.menu = menu_icon[entry.source.name]
-      return item
-    end,
-  },
-  mapping = {
-    -- ["<C-k>"] = cmp.mapping.scroll_docs(-4),
-    -- ["<C-j>"] = cmp.mapping.scroll_docs(4),
+			item.menu = menu_icon[entry.source.name]
+			return item
+		end,
+	},
+	mapping = {
+		["<c-h>"] = cmp.mapping.scroll_docs(4),
+		["<c-l>"] = cmp.mapping.scroll_docs(-4),
 
-    ["<C-x>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+		["<C-x>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
 
-    ["<C-l>"] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(1) then
-        luasnip.jump(1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+		-- ["<C-l>"] = cmp.mapping(function(fallback)
+		-- 	if luasnip.jumpable(1) then
+		-- 		luasnip.jump(1)
+		-- 	else
+		-- 		fallback()
+		-- 	end
+		-- end, { "i", "s" }),
+		--
+		-- ["<C-h>"] = cmp.mapping(function(fallback)
+		-- 	if luasnip.jumpable(-1) then
+		-- 		luasnip.jump(-1)
+		-- 	else
+		-- 		fallback()
+		-- 	end
+		-- end, { "i", "s" }),
 
-    ["<C-h>"] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			local col = vim.fn.col(".") - 1
 
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      local col = vim.fn.col(".") - 1
+			if cmp.visible() then
+				cmp.select_next_item(select_opts)
+			elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+				fallback()
+			else
+				cmp.complete()
+			end
+		end, { "i", "s" }),
 
-      if cmp.visible() then
-        cmp.select_next_item(select_opts)
-      elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-        fallback()
-      else
-        cmp.complete()
-      end
-    end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item(select_opts)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item(select_opts)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+		["<c-j>"] = cmp.mapping(function(fallback)
+			local col = vim.fn.col(".") - 1
 
-    ["<c-j>"] = cmp.mapping(function(fallback)
-      local col = vim.fn.col(".") - 1
+			if cmp.visible() then
+				cmp.select_next_item(select_opts)
+			elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+				fallback()
+			else
+				cmp.complete()
+			end
+		end, { "i", "s" }),
 
-      if cmp.visible() then
-        cmp.select_next_item(select_opts)
-      elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-        fallback()
-      else
-        cmp.complete()
-      end
-    end, { "i", "s" }),
+		["<c-k>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item(select_opts)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<c-n>"] = cmp.mapping(function(fallback)
+			local col = vim.fn.col(".") - 1
 
-    ["<c-k>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item(select_opts)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<c-n>"] = cmp.mapping(function(fallback)
-      local col = vim.fn.col(".") - 1
+			if cmp.visible() then
+				cmp.select_next_item(select_opts)
+			elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+				fallback()
+			else
+				cmp.complete()
+			end
+		end, { "i", "s" }),
 
-      if cmp.visible() then
-        cmp.select_next_item(select_opts)
-      elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-        fallback()
-      else
-        cmp.complete()
-      end
-    end, { "i", "s" }),
-
-    ["<c-p>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item(select_opts)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  },
+		["<c-p>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item(select_opts)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	},
 })
 
 local sign = function(opts)
-  vim.fn.sign_define(opts.name, {
-    texthl = opts.name,
-    text = opts.text,
-    numhl = "",
-  })
+	vim.fn.sign_define(opts.name, {
+		texthl = opts.name,
+		text = opts.text,
+		numhl = "",
+	})
 end
 
 sign({ name = "DiagnosticSignError", text = "✘" })
